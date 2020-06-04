b51827bc268eecff4b38e628c97a60b3393965a6
ito <yorizo.ito@gmail.com>
2020-06-04 16:17:13 +0000

    add column

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -42,11 +42,13 @@ class MessagesController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            'content'=>'required|max:255'
+        $this->validate($request,[
+            'title'=>'required|max:191',
+            'content'=>'required|max:255',
         ]);
         
         $message=new Message;
+        $message->title=$request->title;
         $message->content=$request->content;
         $message->save();
         
@@ -90,8 +92,9 @@ class MessagesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $request->validate([
-            'content'=>'required|max:255'
+        $this->validate($request,[
+            'title'=>'required|max:191',
+            'content'=>'required|max:255',
         ]);
         
         $message=Message::findOrFail($id);
--- resources/views/messages/edit.blade.php
+++ resources/views/messages/edit.blade.php
@@ -5,11 +5,19 @@
     <div class="row">
         <div class="col-6">
             {!! Form::model($message,['route'=>['messages.update',$message->id],'method'=>'put']) !!}
+                
+                <div class="form-group">
+                    {!! Form::label('title','タイトル：') !!}
+                    {!! Form::text('title',null,['class'=>'form-control']) !!}
+                </div>
+                
                 <div class="form-group">
                     {!! Form::label('content','メッセージ：') !!}
                     {!! Form::text('content',null,['class'=>'form-control']) !!}
                 </div>
+                
                 {!! Form::submit('更新',['class'=>'byn btn-primary']) !!}
+                
             {!! Form::close() !!}
         </div>
     </div>
